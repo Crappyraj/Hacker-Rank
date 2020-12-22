b52ee2aeaf163d4e728aef94dd0f8c98dd1cbe78
Crappyraj <67316208+Crappyraj@users.noreply.github.com>
2020-12-22 19:28:56 +0530

    Add files via upload

---
+++ inputoutput.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b,c,sum=0;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    cin>>a>>b>>c;
+    sum=a+b+c;
+    cout<<sum;
+    
+      
+    return 0;
+}
